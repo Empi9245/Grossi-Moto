5b6f9f69ca17c8b6f66bc5a794e778b4de5846b0
Empi9245 <emilparlog01@gmail.com>
2026-09-17 14:52:01 +0200

    Soften home section transition into experience

--- src/components/sections/GrossimotoExperienceSection.tsx
+++ src/components/sections/GrossimotoExperienceSection.tsx
@@ -2,8 +2,10 @@ import { ZoomParallax } from "@/components/ui/zoom-parallax";
 
 export function GrossimotoExperienceSection() {
   return (
-    <section className="relative bg-[var(--home-experience-surface)]">
-      <ZoomParallax />
+    <section className="relative bg-white pt-10 sm:pt-14 lg:pt-24">
+      <div className="bg-[var(--home-experience-surface)]">
+        <ZoomParallax />
+      </div>
     </section>
   );
 }
